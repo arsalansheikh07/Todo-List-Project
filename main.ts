16a32109cefd954a706d546bdf2686b703602a78
Arsalan Sheikh <160051126+arsalansheikh07@users.noreply.github.com>
2024-04-03 23:10:11 +0500

    commit

---
+++ main.ts
@@ -0,0 +1,95 @@
+import { log } from "console";
+import inquirer from "inquirer";
+import { todo } from "node:test";
+// inqurier done
+// array done
+// function done
+// operators
+
+let todos : string[] = ["arsalan","kamran",]
+
+async function creatTodo(todos:string[]){
+    do{
+        let ans = await inquirer.prompt({
+            type: "list",
+            message : "select an operation",
+            name: "select",
+            choices: ["Add","Update","View","Delete",]
+        })
+        if (ans.select == "Add"){
+            let addTodo = await inquirer.prompt({
+                type: "input",
+                message : "add iteam in the list",
+                name: "todo",
+            });
+            todos.push(addTodo.todo);
+            todos.forEach(todo => console.log(todo));
+           // console.log(addTodo);
+    
+        }
+    
+        if (ans.select == "Update"){
+            let updateTodo = await inquirer.prompt({
+                type: "list",
+                message : "update iteam in the list",
+                name: "todo",
+                choices: todos.map(iteam => iteam)
+            });
+            let addTodo = await inquirer.prompt({
+                type: "input",
+                message : "add iteam in the list",
+                name: "todo",
+            });
+            let newTodo = todos.filter(val =>val !== updateTodo.todo);
+            todos = [... newTodo,addTodo.todo];
+            console.log(todos);
+            
+        }
+    
+         if (ans.select == "View"){
+            console.log("*** TO DO LIST ***");
+            console.log(todos);
+            console.log("**********************");
+    
+         }
+    
+    
+         if (ans.select == "Delete"){
+            let deleteTodo = await inquirer.prompt({
+                type: "list",
+                message : "update iteam in the list",
+                name: "todo",
+                choices: todos.map(iteam => iteam)
+            });
+            let newTodo = todos.filter(val =>val !== deleteTodo.todo);
+            todos = [... newTodo];
+            console.log(todos);
+    
+         }
+
+    } while(true)
+    
+}
+
+creatTodo(todos);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
